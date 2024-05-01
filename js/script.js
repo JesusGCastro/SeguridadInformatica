79e609a5c179fd344c385deebced93d339f53825
JesusGCastro <105814927+JesusGCastro@users.noreply.github.com>
2024-04-30 20:06:48 -0700

    Encriptado y desencriptado listo
    Ya quedo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 const formulario = document.getElementById('contactForm');
+const formularioDesencriptado = document.getElementById('contactForm2');
 const botonSubmit = document.getElementById('botonDES');
+
+// Campos para encriptar
 const mensajeClaro = document.getElementById('mensajeCLARO');
-let mensajeCifrado = document.getElementById('mensajeCIFRADO'); 
+const mensajeCifrado = document.getElementById('mensajeCIFRADO'); 
 
 document.addEventListener('click', function() {
     // Obtener el formulario y el botón de enviar
@@ -19,11 +20,39 @@ document.addEventListener('click', function() {
         const mensaje = mensajeClaro.value;
 
         // Cifra el mensaje usando CryptoJS
-        let mensajeFinal = CryptoJS.DES.encrypt(mensaje, key).toString();
+        const mensajeFinal = CryptoJS.DES.encrypt(mensaje,key,{
+            mode: CryptoJS.mode.ECB,
+            padding: CryptoJS.pad.Pkcs7
+        });
 
         // Actualiza el contenido del textarea con el mensaje cifrado
-        mensajeCifrado = mensajeFinal;
+        mensajeCifrado.value = mensajeFinal;
+    });
+});
 
-        console.log(mensajeCifrado);
+// Campos para encriptar
+const cifradoMensaje = document.getElementById('mensajeCIFRADO2');
+const claroMensaje = document.getElementById('mensajeCLARO2'); 
+
+document.addEventListener('click', function() {
+    // Obtener el formulario y el botón de enviar
+    // Agregar un listener para el evento submit del formulario
+    formularioDesencriptado.addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        // Definir la key para descifrar
+        const key = "CTBroRealmenteEstaEsLaClaveMontana";
+        
+        // Obtén el contenido del textarea
+        const textoADescifrar = cifradoMensaje.value;
+
+        // Cifra el mensaje usando CryptoJS
+        const mensajeFinal = CryptoJS.DES.decrypt(textoADescifrar,key,{
+            mode: CryptoJS.mode.ECB,
+            padding: CryptoJS.pad.Pkcs7
+        });
+
+        // Actualiza el contenido del textarea con el mensaje cifrado
+        claroMensaje.value = mensajeFinal.toString(CryptoJS.enc.Utf8);
     });
 });
